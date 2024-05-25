c21e600c5031167a4ab98b5c5f37abf7c88af0d5
IChris2611I <pachris2611@gmail.com>
2024-05-25 18:15:20 +0200

    Setting up : Router / Setting up : Project Structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import ErrorPage from "./pages/ErrorPage";
+import HomePage from "./pages/HomePage";
+
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,5 @@
+function HomePage() {
+  return <h1>Hello Chris ! This is HomePage</h1>;
+}
+
+export default HomePage;
