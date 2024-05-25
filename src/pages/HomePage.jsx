fed4a3ae21700230578feded601920c90b06b06b
IChris2611I <pachris2611@gmail.com>
2024-05-25 18:52:18 +0200

    BGImage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,13 @@
 function HomePage() {
-  return <h1>Hello Chris ! This is HomePage</h1>;
+  return (
+    <div
+      className="relative h-screen bg-cover bg-center"
+      style={{ backgroundImage: "url('/src/assets/background.png')" }}
+    >
+      <div className="absolute inset-0 bg-slate-800 opacity-90"></div>
+      <div className="relative z-10"></div>
+    </div>
+  );
 }
 
 export default HomePage;
