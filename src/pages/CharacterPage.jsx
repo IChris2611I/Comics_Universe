d0603ddd5fa3bb4c5d919a2041c810136464b498
IChris2611I <pachris2611@gmail.com>
2024-05-26 01:06:47 +0200

    Opti Background

--- src/pages/CharacterPage.jsx
+++ src/pages/CharacterPage.jsx
@@ -1,20 +1,18 @@
 const CharacterPage = () => {
   return (
-    <div className="w-screen h-screen bg-[#03223D]">
+    <div className="w-screen h-screen bg-[#03223D] relative">
       <div
-        className="absolute inset-0 bg-fixed bg-center bg-no-repeat bg-cover opacity-10"
+        className="absolute inset-0 bg-fixed bg-center bg-no-repeat bg-cover opacity-5"
         style={{ backgroundImage: "url('/src/assets/bgWW.png')" }}
-      >
-        <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-neutral-50  to-neutral-950 opacity-20">
-          <div className="flex h-screen">
-            <div className="w-4/5 max-h-full grow">
-              <div className="h-20 text-white">test</div>
-            </div>
-            <div className="w-1/5 max-h-full border-l-2 border-[#A4A4A4] grow0  ">
-              <div className="h-20 border-b-2 border-[#A4A4A4] text-white">
-                test2
-              </div>
-            </div>
+      ></div>
+      <div className="absolute inset-0 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-neutral-50  to-neutral-950 opacity-10"></div>
+      <div className="relative flex h-full">
+        <div className="flex-grow w-4/5 max-h-full">
+          <div className="h-20 text-white">test</div>
+        </div>
+        <div className="w-1/5 max-h-full border-l-2 border-[#ffffff22] flex-grow-0">
+          <div className="h-20 border-b-2 border-[#ffffff22] text-white">
+            test2
           </div>
         </div>
       </div>
